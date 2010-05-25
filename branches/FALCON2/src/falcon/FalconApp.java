02afded0425921f35b3ded17bb1f7db89a56e300
ethanharstad <ethanharstad@ab725d98-2ea6-cbe9-5cc1-74d4d5e333da>
2010-05-25 13:56:02 +0000

    GUI changes

--- branches/FALCON2/src/falcon/FalconApp.java
+++ branches/FALCON2/src/falcon/FalconApp.java
@@ -1,17 +1,18 @@
 package falcon;
 
-import java.awt.Frame;
-import java.awt.GraphicsEnvironment;
-
+import java.awt.Dimension;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
-
-import falcon.components.gui.*;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class FalconApp extends JFrame{
 	
 	public final static String VERSION = "Pre-Alpha Branch";
 	
-	static JFrame window;
+	static JFrame mainWindow;
+	static JMenuBar cMenuBar;
 
 	/**
 	 * @param args
@@ -27,22 +28,48 @@ public class FalconApp extends JFrame{
 	}
 	
 	private static void createWindow() {
-		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		
-		window = new JFrame("FALCON " + VERSION + " - Iowa State University SSCL");
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setMaximizedBounds(env.getMaximumWindowBounds());
-		window.setExtendedState(window.getExtendedState() | Frame.MAXIMIZED_BOTH);
+		mainWindow = new JFrame("FALCON " + VERSION + " - Iowa State University SSCL");
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.setMinimumSize(new Dimension(600,400));
+		mainWindow.setPreferredSize(new Dimension(600,400));
 		
 		//TODO Add components
-		LogPanel console = new LogPanel();
-		window.add(console);
-		console.addMessage("Testing the log panel!");
-		console.addEvent("A random event has happened!");
-		console.addWarning("Something bad is about to happen!");
-		console.addAlert("A noes! Something bad has happened!");
+		mainWindow.setJMenuBar(createMenuBar());
 		
-		window.setVisible(true);
+		mainWindow.setVisible(true);
+	}
+	
+	private static JMenuBar createMenuBar() {
+		cMenuBar = new JMenuBar();
+		
+		JMenu cFileMenu = new JMenu("File");
+		cMenuBar.add(cFileMenu);
+		JMenuItem cFileExit = new JMenuItem("Exit");
+		cFileMenu.add(cFileExit);
+		
+		JMenu cWindowMenu = new JMenu("Window");
+		cMenuBar.add(cWindowMenu);
+		JCheckBoxMenuItem cWindowRadio = new JCheckBoxMenuItem("Radio");
+		cWindowMenu.add(cWindowRadio);
+		JCheckBoxMenuItem cWindowTracking = new JCheckBoxMenuItem("Tracking");
+		cWindowMenu.add(cWindowTracking);
+		JCheckBoxMenuItem cWindowPrediction = new JCheckBoxMenuItem("Prediction");
+		cWindowMenu.add(cWindowPrediction);
+		JCheckBoxMenuItem cWindowMap = new JCheckBoxMenuItem("Map");
+		cWindowMenu.add(cWindowMap);
+		JCheckBoxMenuItem cWindowTelemetry = new JCheckBoxMenuItem("Telemetry");
+		cWindowMenu.add(cWindowTelemetry);
+		JCheckBoxMenuItem cWindowStatus = new JCheckBoxMenuItem("Status");
+		cWindowMenu.add(cWindowStatus);
+		
+		JMenu cHelpMenu = new JMenu("Help");
+		cMenuBar.add(cHelpMenu);
+		JMenuItem cHelpHelp = new JMenuItem("Help");
+		cHelpMenu.add(cHelpHelp);
+		JMenuItem cHelpAbout = new JMenuItem("About");
+		cHelpMenu.add(cHelpAbout);
+		
+		return cMenuBar;
 	}
 
 }
