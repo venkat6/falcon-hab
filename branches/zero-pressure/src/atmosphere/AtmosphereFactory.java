8159a3d9eabede76f3fb2385a1d1eccdc61a5f5a
ethanharstad <ethanharstad@ab725d98-2ea6-cbe9-5cc1-74d4d5e333da>
2010-10-25 23:04:49 +0000

    AtmosphereModel interpolation implemented, not yet tested. AtmosphereFactory GFS parsing implemented, not yet tested.

--- branches/zero-pressure/src/atmosphere/AtmosphereFactory.java
+++ branches/zero-pressure/src/atmosphere/AtmosphereFactory.java
@@ -1,11 +1,16 @@
 package atmosphere;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public class AtmosphereFactory {
 	
@@ -15,13 +20,10 @@ public class AtmosphereFactory {
 		AtmosphereProfile startState = getGfsProfile(startTime, lat, lon);
 		AtmosphereProfile endState = getGfsProfile(endTime, lat, lon);
 		
-		AtmosphereModel model = new AtmosphereModel();
-		//TODO
-		return model;
+		return new AtmosphereModel(startState, endState);
 	}
 	
 	public static AtmosphereProfile getGfsProfile(int time, double lat, double lon) {
-		//TODO
 		time = (time/AtmosphereProfile.mTimeStep)*AtmosphereProfile.mTimeStep;
 		// connect to url
 		String address = 	"http://rucsoundings.noaa.gov/get_soundings.cgi?data_source=GFS;airport="+lat+","+lon+
@@ -43,20 +45,50 @@ public class AtmosphereFactory {
 		BufferedReader br = new BufferedReader(isr);
 		// create a file to store the data
 		String filename = Integer.toString(time/1000)+"_"+Integer.toString((int)(lat*100))+"_"+Integer.toString((int)(lon*100))+".dat";
+		FileOutputStream out;
+		PrintStream p = null;
+		try {
+			out = new FileOutputStream(filename);
+			p = new PrintStream(out);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 		// read data, format and store to file
-		
-		
+		//TODO
 		// close the file
-		
+		p.close();
 		// parse the file and return the profile
 		return parseGfsProfile(filename);
 	}
 	
 	public static AtmosphereProfile parseGfsProfile(String filename) {
 		AtmosphereProfile atmo = new AtmosphereProfile();
-		
-		//TODO
-		
+		FileInputStream is = null;
+		try {
+			is = new FileInputStream(filename);
+		} catch (FileNotFoundException e) {
+			System.err.println("Could not find file: " + filename);
+			e.printStackTrace();
+			return atmo;
+		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String line = null;
+		try {
+			while((line = br.readLine()) != null) {
+				Scanner parser = new Scanner(line);
+				double p = parser.nextDouble();
+				double a = parser.nextDouble();
+				double t = parser.nextDouble();
+				double dp = parser.nextDouble();
+				double dir = parser.nextDouble();
+				double spd = parser.nextDouble();
+				atmo.addData(p, a, t, dp, dir, spd);
+			}
+		} catch (IOException e) {
+			System.err.println("Problem reading file: " + filename);
+			e.printStackTrace();
+			return atmo;
+		}		
 		return atmo;
 	}
 
--- branches/zero-pressure/src/atmosphere/AtmosphereProfile.java
+++ branches/zero-pressure/src/atmosphere/AtmosphereProfile.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 
+
 public class AtmosphereProfile {
 	
 	protected int mStartTime;
