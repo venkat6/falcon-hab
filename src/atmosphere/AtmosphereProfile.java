a826e478231481047beaafee4c3182283ade6123
ethanharstad <ethanharstad@ab725d98-2ea6-cbe9-5cc1-74d4d5e333da>
2010-10-27 01:55:39 +0000

    Wind data downloading finished and tested. Parsing implemented but still untested, no reason to suspect it not to work

--- src/atmosphere/AtmosphereProfile.java
+++ src/atmosphere/AtmosphereProfile.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 
-
 public class AtmosphereProfile {
 	
 	protected int mStartTime;
--- src/atmosphere/AtmosphereTest.java
+++ src/atmosphere/AtmosphereTest.java
@@ -3,10 +3,7 @@ package atmosphere;
 public class AtmosphereTest {
 
 	public static void main(String[] args) {
-		AtmosphereProfile atmo = new AtmosphereProfile();
-		atmo.addData(1,1,0,0,0,0);
-		atmo.addData(0,0,0,0,0,0);
-		
+		AtmosphereFactory.getGfsModel(1288126800,41.9,-93.6);
 	}
 	
 }
